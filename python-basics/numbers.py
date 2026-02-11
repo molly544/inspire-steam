d6b99763e07d4b73fb79781b515fd701272c10d8
molly544 <mollymbugua123456@gmail.com>
2026-02-11 10:55:00 +0300

    programme to add,subtract,divide,multiply

---
+++ python-basics/numbers.py
@@ -0,0 +1,17 @@
+#Maureen Mbugua
+#11/02/2026
+#programme to add,subtract,divide,multiply
+
+first_number=(int{input"enter first_number"})
+second_number=(int{input"enter second_number"})
+
+sum_numbers=first number+second number 
+difference_numbers=first number-second number
+quotient_numbers=first number/second number
+product_numbers=first number*second number
+
+print(f"sum_numbers")
+print(f"difference_numbers")
+print(f"quotient_numbers")
+print(f"product_numbers")
+
---
+++ python-basics/user_input.py
@@ -0,0 +1,7 @@
+#Maureen Mbugua
+#11/02/2026
+#programme to get user input
+
+name=input("what is your name")
+
+print(f"name")
