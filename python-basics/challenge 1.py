ea7fc2f175e3dfa576d9c21975ba59d172f1ef10
molly544 <mollymbugua123456@gmail.com>
2026-02-11 09:29:16 +0300

    first commit

---
+++ python-basics/challenge 1.py
@@ -0,0 +1,9 @@
+university="JKUAT"
+course="data_analysis"
+grade="B"
+cluster_points="67"
+
+print(f"my favourite university is{university}")
+print(f"my favourite course is{course}")
+print(f"my grade is{grade}")
+print(f"my cluster_points are{cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,13 @@
+#this is a comment
+print("hello from Maureen")
+#city/town
+print("i am from kiambu")
+#my hobbies
+print("i like listening to music,sleeping and reading novels")
+age=19
+height=1.25
+weight=62
+
+print(f"i am {age}years old")
+print(f"i am {height}metres")
+print(f"i am {weight}kgs")
