35ec0d057f46a62c218626d4d8230eecc3551642
molly544 <mollymbugua123456@gmail.com>
2026-02-16 09:53:27 +0300

    while loop

--- python-basics/challenge 2.py
+++ python-basics/challenge 2.py
@@ -2,5 +2,12 @@
 #16/02/2026
 #program to show for loops in python
 import math
+from tabulate import tabulate
 
-how to make table
+angles=(0,30,45,60,90)
+data[]
+for angle in angles:
+    rad=math.radians(angle)
+    data.append([angle,round(math.sin(rad),4),round(math.cos(rad),4),round(math.tan(rad),4)])
+
+print(f(tabulate{data,headers=['angle','sin','cos','tan'],tablefmt='grid'}))
---
+++ python-basics/while.py
@@ -0,0 +1,8 @@
+#Maureen Mbugua
+#16/02/2026
+#programme to display  numbers using while loop
+
+number=-1
+while number <10:
+    number = number+1
+    print(number)
