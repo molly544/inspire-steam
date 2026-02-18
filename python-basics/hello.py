76d8157446e2d8c7c13117957312d8045f0d6b92
Reenie_. <mollymbugua123456@gmail.com>
2026-02-18 14:30:58 +0300

    Update print statements and variable values in hello.py

--- python-basics/hello.py
+++ python-basics/hello.py
@@ -1,12 +1,12 @@
 #this is a comment
-print("hello from Maureen")
+print("hello from Susan")
 #city/town
-print("i am from kiambu")
+print("i am from Murang'a")
 #my hobbies
-print("i like listening to music,sleeping and reading novels")
-age=19
-height=1.25
-weight=62
+print("i like listening to music,eating,sleeping and reading novels")
+age=17
+height=1.35
+weight=66
 
 print(f"i am {age}years old")
 print(f"i am {height}metres")
