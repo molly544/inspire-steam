f4fefda19f0a540343d841dc37e696b388ebe4e7
molly544 <mollymbugua123456@gmail.com>
2026-02-18 13:27:00 +0300

    myself

---
+++ python-basics/Reenie.py
@@ -0,0 +1,33 @@
+#Maureen Mbugua
+#18/02/2026
+#programme to 
+
+university="JKUAT"
+course="data_analysis"
+sport="tennis"
+hobby="reading"
+food="pizza"
+values="patient,nice"
+trait="optimistic"
+
+
+
+
+print(f"my favourite university is{university}")
+print(f"my favourite course is{course}")
+print(f"my favourite hobbyis{hobby}")
+print(f"my favourite food is{food}")
+print(f"my favourite sport is{sport}")
+print(f"my values are{values}")
+print(f"my trait is{trait}")
+
+
+
+
+
+
+
+
+
+
+
